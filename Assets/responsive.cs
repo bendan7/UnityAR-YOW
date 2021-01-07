9c1295e64e0cb4b5558538da95c0597f63af9435
Ben Dan <bendan7@gmail.com>
2021-01-07 13:53:28 +0200

    add buttons and rotaion script

---
+++ Assets/responsive.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+[ExecuteInEditMode]
+public class responsive : MonoBehaviour
+{
+
+    private ScreenOrientation _screenOrientation = ScreenOrientation.Unknown;
+    public GridLayoutGroup gridLayoutGroup;
+
+    void Start()
+    {
+
+
+    }
+
+
+    void Update()
+    {
+        if (Screen.width > Screen.height)
+        {
+            SetLandscape();
+        }
+        else
+        {
+            SetPortarit();
+        }
+    }
+
+    private void SetPortarit()
+    {
+        if (_screenOrientation == ScreenOrientation.Portrait)
+        {
+            return;
+        }
+
+        gridLayoutGroup.cellSize = new Vector2(380, 200);
+        _screenOrientation = ScreenOrientation.Portrait;
+        Debug.Log("Portarit");
+
+
+    }
+
+    private void SetLandscape()
+    {
+        if (_screenOrientation == ScreenOrientation.Landscape)
+        {
+            return;
+        }
+
+        gridLayoutGroup.cellSize = new Vector2(380, 85);
+        _screenOrientation = ScreenOrientation.Landscape;
+        Debug.Log("Landscape");
+
+    }
+}
---
+++ Assets/rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        // this.transform.Rotate = new Quaternion(transform.rotation.x, transform.rotation.y + 0.01f, transform.rotation.z, transform.rotation.w);
+        transform.Rotate(0, 0.5f, 0, Space.Self);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
